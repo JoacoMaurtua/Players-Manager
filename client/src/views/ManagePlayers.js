970e82b8a1a2b1eebb26dc3c4b3f0e193b9a1da6
JoacoMaurtua <joacomaurtua@gmail.com>
2021-09-30 17:18:30 -0500

    manage players structure complete

--- client/src/views/ManagePlayers.js
+++ client/src/views/ManagePlayers.js
@@ -13,6 +13,30 @@ const ManagePlayers = ({list,setList}) => {
       <Link to={'/status'}>
           <h3>Manage player Status</h3>
       </Link>
+      <Table striped>
+            <thead>
+                <tr>
+                    <th style={{textAlign: 'center'}}>Player Name</th>
+                    <th style={{textAlign: 'center'}}>Preferred Position</th>
+                    <th style={{textAlign: 'center'}}>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {list&&list.map((items, index) => (
+                    <tr key={index}>
+                        <td style={{textAlign: 'center'}}>{items.name}</td>
+                        <td style={{textAlign: 'center'}}>{items.position}</td>
+                        <td style={{textAlign: 'center'}}>
+                            <Row>
+                              <Col md={12}>
+                                  <Button color="danger">Eliminar</Button>
+                              </Col>
+                            </Row>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table> 
       
     </div>
   );
